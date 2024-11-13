491a0ca3ed972e3c8f7c1806e905d1c47bdc3dc7
Andrea Vismara <andrea.vismara@s.wu.ac.at>
2024-11-13 17:54:23 +0100

    now we have clean yearly age and sex data

---
+++ austria/data/yearly_age_sex.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+import re
+
+folder = "C:\\users\\andrea vismara\\downloads\\age_sex\\"
+
+files = os.listdir(folder)
+
+df = pd.DataFrame()
+for file in tqdm(files):
+    df_ = pd.read_excel(folder + file, skiprows=10, skipfooter=9).iloc[1:, 1:]
+    df_.replace('-', 0, inplace =True)
+    df_.columns = ['year', 'sex', 'age_group'] + [x for x in df_.columns[3:]]
+    df_.ffill(inplace = True)
+    df_ = pd.melt(df_, id_vars=['year', 'sex', 'age_group'], value_vars=[x for x in df_.columns[3:]],
+                  var_name='country', value_name='people')
+    df_ = df_.replace('up to 4 years old', '0 to 4 years old')
+    df_['age_group'] = df_['age_group'].apply(lambda x: [int(s) for s in re.findall(r'\b\d+\b', x)])
+    df = pd.concat([df, df_])
+
+df.to_excel("c:\\data\\population\\austria\\age_sex_all_clean.xlsx", index = False)
