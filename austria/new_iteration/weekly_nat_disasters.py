073f4e0fb43ccaae59b4d3c5e24a6d56710cdbc7
Andrea Vismara <andrea.vismara@s.wu.ac.at>
2025-01-22 14:45:05 +0100

    weekly disasters data

--- austria/new_iteration/weekly_nat_disasters.py
+++ austria/new_iteration/weekly_nat_disasters.py
@@ -7,6 +7,7 @@ Description: creates a global dataset of natural disaster events at the weekly l
 
 #imports
 import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objects as go
@@ -15,15 +16,7 @@ import plotly.express as px
 import os
 import json
 pio.renderers.default = 'browser'
-
-dict_names = {}
-with open('c:\\data\\general\\countries_dict.txt',
-          encoding='utf-8') as f:
-    data = f.read()
-js = json.loads(data)
-for k,v in js.items():
-    for x in v:
-        dict_names[x] = k
+from utils import dict_names
 
 #read the EM-DAT data
 emdat = pd.read_excel("c:\\data\\natural_disasters\\emdat_2024_07_all.xlsx")
@@ -54,3 +47,36 @@ df_nd.drop(columns = ['End Year','End Month', 'End Day', 'Start Year','Start Mon
 df_nd.rename(columns = {'Country' : 'country', "total affected" : "total_affected"}, inplace = True)
 
 df_nd.to_excel("C:\\Data\\natural_disasters\\disasters_start_end_dates.xlsx", index = False)
+
+def spread_disasters(df):
+    expanded_rows = []
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        # Generate weekly intervals between start and end dates
+        date_range = pd.date_range(start=row['start_date'], end=row['end_date'], freq='W')
+        num_weeks = len(date_range)
+        if num_weeks == 0:
+            # If the event lasts less than a week, assign it to the start date's week
+            expanded_rows.append({
+                'country': row['country'],
+                'week_start': row['start_date'],
+                'total_affected': row['total_affected']
+            })
+        else:
+            # Distribute deaths evenly across weeks
+            deaths_per_week = row['total_affected'] / num_weeks
+            for week_start in date_range:
+                expanded_rows.append({
+                    'country': row['country'],
+                    'week_start': week_start,
+                    'total_affected': deaths_per_week
+                })
+    return pd.DataFrame(expanded_rows)
+
+# Expand the DataFrame
+expanded_df = spread_disasters(df_nd)
+
+# Group by country and week, summing the deaths
+weekly_disasters = expanded_df.groupby(['country', 'week_start']).sum().reset_index()
+weekly_disasters['country'] = weekly_disasters.country.map(dict_names)
+
+weekly_disasters.to_csv("C:\\Data\\my_datasets\\weekly_disasters.csv", index = False)
