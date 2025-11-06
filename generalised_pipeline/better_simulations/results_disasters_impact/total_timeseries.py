73b2baf46aec35a370539c025907a294bc855a18
Andrea Vismara <andrea.vismara@s.wu.ac.at>
2025-11-06 10:56:49 +0100

    updated total results calculation

--- generalised_pipeline/better_simulations/results_disasters_impact/total_timeseries.py
+++ generalised_pipeline/better_simulations/results_disasters_impact/total_timeseries.py
@@ -24,7 +24,7 @@ df_rem_mex.rename(columns = {'total_mln' : 'remittances'}, inplace = True)
 df_rem_mex['remittances'] *= 1_000_000
 
 ## Diaspora numbers
-diasporas_file = "C:\\Data\\migration\\bilateral_stocks\\interpolated_stocks_and_dem_factors_2207_TRAIN.pkl"
+diasporas_file = "C:\\Data\\migration\\bilateral_stocks\\interpolated_stocks_and_dem_factors_2210.pkl"
 df = pd.read_pickle(diasporas_file)
 df = df.dropna()
 
@@ -37,10 +37,16 @@ emdat = pd.read_pickle("C:\\Data\\my_datasets\\monthly_disasters_with_lags_NEW.p
 
 ####parameters
 
-params = [2.323125219174453, -8.90268722949454, 9.158294373208719,
-            0.1788188275520765, 0.21924650014050806,-0.7500114294211869,
-            -0.04579122940366171, 0.13737550869522205]
-param_nta, param_asy, param_gdp, height, shape, shift, constant, rem_pct = params
+params = [np.float64(1.1419923789286681),
+ np.float64(-5.263671004897209),
+ np.float64(0.5683594794155823),
+ np.float64(-3.0),
+ np.float64(0.11572276929321759),
+ np.float64(0.2706053347359994),
+ np.float64(-1.0),
+ np.float64(-0.01074182205082303),
+ np.float64(0.1957812640316911)]
+param_nta, param_asy, param_gdp, param_inc, height, shape, shift, constant, rem_pct = params
 
 ######## functions
 def calculate_tot_score(emdat_ita, height, shape, shift):
@@ -82,9 +88,9 @@ def simulate_remittances(df_countries, height, shape, shift, rem_pct, disasters 
 
     df_countries['rem_amount'] = rem_pct * df_countries['gdp'] / 12
 
-    df_countries['theta'] = constant + (param_nta * (df_countries['nta'])) \
-                            + (param_asy * df_countries['asymmetry']) + (param_gdp * df_countries['gdp_diff_norm']) \
-                            + (df_countries['tot_score'])
+    df_countries['theta'] = constant + (param_nta * (df_countries['nta'])) + (param_inc * (df_countries["gdp_origin_norm"])) \
+                    + (param_asy * df_countries['asymmetry']) + (param_gdp * df_countries['relative_diff']) \
+                    + (df_countries['tot_score'])
     df_countries['probability'] = 1 / (1 + np.exp(-df_countries["theta"]))
     df_countries.loc[df_countries.nta == 0, 'probability'] = 0
     df_countries['sim_senders'] = (df_countries['probability'] * df_countries['n_people']).astype(int)
@@ -108,9 +114,8 @@ def simulate_remittances_specific_disaster(df_countries, height, shape, shift, r
     df_countries[f'{dis_name}_score'].fillna(0, inplace = True)
 
     df_countries['rem_amount'] = rem_pct * df_countries['gdp'] / 12
-
-    df_countries['theta'] = constant + (param_nta * (df_countries['nta'])) \
-                            + (param_asy * df_countries['asymmetry']) + (param_gdp * df_countries['gdp_diff_norm']) \
+    df_countries['theta'] = constant + (param_nta * (df_countries['nta'])) + (param_inc * (df_countries["gdp_origin_norm"]))\
+                            + (param_asy * df_countries['asymmetry']) + (param_gdp * df_countries['relative_diff']) \
                             + (df_countries[f'{dis_name}_score'])
     df_countries['probability'] = 1 / (1 + np.exp(-df_countries["theta"]))
     df_countries.loc[df_countries.nta == 0, 'probability'] = 0
@@ -158,39 +163,38 @@ def quick_check_mex_(df_countries, height, shape, shift, rem_pct, plot = True, d
 
 ##### run simulations
 # WITH DISASTERS
-biggest_countries = df[["origin", "n_people"]].groupby('origin').sum().sort_values('n_people', ascending = False)[:20].index.tolist()
+# biggest_countries = df[["origin", "n_people"]].groupby('origin').sum().sort_values('n_people', ascending = False)[:20].index.tolist()
 
 print("simulating all disasters ....")
 df_results = simulate_remittances(df, height, shape, shift, rem_pct, disasters = True)
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_with_disasters_CORRECT.parquet")
-# WITHOUT
+df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_with_disasters_NEW.parquet")
+# # WITHOUT
 print("simulating NO disasters ...")
 df_results = simulate_remittances(df, height, shape, shift, rem_pct, disasters = False)
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_without_disasters_CORRECT.parquet")
-# DROUGHT
-print("simulating droughts ...")
-df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Drought")
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\CORRECT_drought.parquet")
-# FLOOD
-print("simulating floods ...")
-df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Flood")
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\CORRECT__floods.parquet")
-# WITHOUT
-print("simulating storms ...")
-df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Storm")
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\CORRECT__storms.parquet")
-# WITHOUT
-print("simulating earthquakes ...")
-df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Earthquake")
-df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\CORRECT__earthquakes.parquet")
-
+df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_without_disasters_NEW.parquet")
+# # DROUGHT
+# print("simulating droughts ...")
+# df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Drought")
+# df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\NEW_drought.parquet")
+# # FLOOD
+# print("simulating floods ...")
+# df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Flood")
+# df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\NEW_floods.parquet")
+# # WITHOUT
+# print("simulating storms ...")
+# df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Storm")
+# df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\NEW_storms.parquet")
+# # WITHOUT
+# print("simulating earthquakes ...")
+# df_results = simulate_remittances_specific_disaster(df, height, shape, shift, rem_pct, disaster="Earthquake")
+# df_results.to_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\NEW_earthquakes.parquet")
 
 #################
 # plot
 #################
 
-df_with = pd.read_pickle("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_with_disasters_2307.pkl")
-df_without = pd.read_pickle("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_without_disasters_2307.pkl")
+df_with = pd.read_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_with_disasters_NEW.parquet")
+df_without = pd.read_parquet("C:\\git-projects\\csh_remittances\\general\\results_plots\\all_flows_simulations_without_disasters_NEW.parquet")
 
 ##########
 # save df_with aggregate
@@ -332,7 +336,7 @@ df_by_country_dest.rename(columns = {'origin' : 'country'}, inplace = True)
 df_by_country_dest = df_by_country_dest.merge(df_in_country, on = 'country', how = 'left')
 df_by_country_dest['difference'] = df_by_country_dest['sim_remittances_with'] - df_by_country_dest['sim_remittances_without']
 df_by_country_dest['pct_difference'] = round(100 * df_by_country_dest['difference'] / df_by_country_dest['sim_remittances_without'],2)
-df_by_country_dest.sort_values('difference', ascending = False, inplace = True)
+df_by_country_dest.sort_values('inflow', ascending = False, inplace = True)
 
 from tabulate import tabulate
 print(tabulate(df_by_country_dest.head(10), headers='keys', tablefmt='pretty'))
@@ -350,8 +354,13 @@ x_log = np.log10(df_filtered['inflow'])
 y_log = np.log10(df_filtered['sim_remittances_with'])
 
 # Fit linear regression in log-log space
+from sklearn.metrics import r2_score
+
 reg = LinearRegression().fit(x_log.values.reshape(-1, 1), y_log.values)
-r2 = reg.score(x_log.values.reshape(-1, 1), y_log.values)
+y_pred_log = reg.predict(x_log.values.reshape(-1, 1))
+r2_log = r2_score(y_log, y_pred_log)
+y_pred = np.exp(y_pred_log)
+r2_original = r2_score(np.exp(y_log), y_pred)
 
 # Create scatter plot
 fig = px.scatter(
@@ -388,7 +397,15 @@ fig.add_annotation(
     xref='paper', yref='paper',
     x=0.05, y=0.95,
     showarrow=False,
-    text=f"R² (log-log): {r2:.3f}",
+    text=f"R² (log-log): {r2_log:.3f}",
+    font=dict(size=14)
+)
+
+fig.add_annotation(
+    xref='paper', yref='paper',
+    x=0.05, y=0.9,
+    showarrow=False,
+    text=f"R² (linear): {r2_original:.3f}",
     font=dict(size=14)
 )
 
@@ -407,35 +424,35 @@ fig.show()
 #################################
 # Maps
 #################################
-from geodatasets import get_path
-import geopandas as gpd
-world = gpd.read_file(get_path("naturalearth.land"))
-world['country'] = world['country'].map(dict_names)
-
-# Merge with GeoDataFrame
-merged = world.merge(df_filtered, how='left', left_on='name', right_on='country')
-
-# Plot
-merged.plot(column='remittances', cmap='Blues', legend=True, figsize=(12, 8))
-plt.title('Remittances Received by Country')
-plt.axis('off')
-plt.show()
-
-fig = px.choropleth(df_filtered,
-                    locations='country',
-                    locationmode='country names',
-                    color='difference',
-                    color_continuous_scale='Greens',
-                    title='Remittances received by country (response to disasters)')
-fig.show()
-
-fig = px.choropleth(df_filtered,
-                    locations='country',
-                    locationmode='country names',
-                    color='sim_remittances_with',
-                    color_continuous_scale='Greens',
-                    title='Remittances received by country (totals)')
-fig.show()
+# from geodatasets import get_path
+# import geopandas as gpd
+# world = gpd.read_file(get_path("naturalearth.land"))
+# world['country'] = world['country'].map(dict_names)
+#
+# # Merge with GeoDataFrame
+# merged = world.merge(df_filtered, how='left', left_on='name', right_on='country')
+#
+# # Plot
+# merged.plot(column='remittances', cmap='Blues', legend=True, figsize=(12, 8))
+# plt.title('Remittances Received by Country')
+# plt.axis('off')
+# plt.show()
+#
+# fig = px.choropleth(df_filtered,
+#                     locations='country',
+#                     locationmode='country names',
+#                     color='difference',
+#                     color_continuous_scale='Greens',
+#                     title='Remittances received by country (response to disasters)')
+# fig.show()
+#
+# fig = px.choropleth(df_filtered,
+#                     locations='country',
+#                     locationmode='country names',
+#                     color='sim_remittances_with',
+#                     color_continuous_scale='Greens',
+#                     title='Remittances received by country (totals)')
+# fig.show()
 
 def plot_one_receiver_senders(country):
     df_dest = (df_with[df_with.origin == country][["date", "origin", "destination", "sim_remittances"]]
@@ -455,7 +472,10 @@ def plot_one_receiver_senders(country):
                       title=f'Timeseries of remittances inflow to {country}')
     fig.show()
 
-plot_one_receiver_senders("United Kingdom")
+# plot_one_receiver_senders("United Kingdom")
 plot_one_receiver_senders("Mexico")
-plot_one_receiver_senders("Bangladesh")
-
+plot_one_receiver_senders("Philippines")
+plot_one_receiver_senders("China")
+plot_one_receiver_senders("India")
+plot_one_receiver_senders("Nigeria")
+plot_one_receiver_senders("France")
